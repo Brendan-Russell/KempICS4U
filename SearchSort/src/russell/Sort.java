c58d02ae416c1aca817b633135df3dff8040f511
Brendan Russell <Brendan.Russell@student.tdsb.on.ca>
2016-12-22 10:05:08 -0500

    Commented sorts

--- SearchSort/src/russell/Sort.java
+++ SearchSort/src/russell/Sort.java
@@ -1,25 +1,52 @@
 package russell;
 
 public class Sort {
+	/**
+	 * Swaps two values in an int array
+	 * @param a the array
+	 * @param i the index of the first element
+	 * @param j	the index of the second element
+	 * @return	the array with the values swapped
+	 */
 	public static int[] swap(int[] a, int i, int j){
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 		return a;
 	}
+	/**
+	 * Swaps two values in a double array
+	 * @param a the array
+	 * @param i the index of the first element
+	 * @param j	the index of the second element
+	 * @return	the array with the values swapped
+	 */
 	public static double[] swap(double[] a, int i, int j){
 		double temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 		return a;
 	}
+	/**
+	 * Swaps two values in a String array
+	 * @param a the array
+	 * @param i the index of the first element
+	 * @param j	the index of the second element
+	 * @return	the array with the values swapped
+	 */
 	public static String[] swap(String[] a, int i, int j){
 		String temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 		return a;
 	}
-
+	
+	/**
+	 * Merges two sorted int arrays
+	 * @param a the first sorted array
+	 * @param b	the second sorted array
+	 * @return the two arrays merged into one sorted array
+	 */
 	private static int[] merge(int[] a, int[] b){
 		int[] c = new int[a.length+b.length];
 		int aIndex = 0, bIndex = 0;
@@ -53,6 +80,12 @@ public class Sort {
 		}
 		return c;
 	}
+	/**
+	 * Merges two sorted double arrays
+	 * @param a the first sorted array
+	 * @param b	the second sorted array
+	 * @return the two arrays merged into one sorted array
+	 */
 	private static double[] merge(double[] a, double[] b){
 		double[] c = new double[a.length+b.length];
 		int aIndex = 0, bIndex = 0;
@@ -86,6 +119,12 @@ public class Sort {
 		}
 		return c;
 	}
+	/**
+	 * Merges two sorted String arrays
+	 * @param a the first sorted array
+	 * @param b	the second sorted array
+	 * @return the two arrays merged into one sorted array
+	 */
 	private static String[] merge(String[] a, String[] b){
 		String[] c = new String[a.length+b.length];
 		int aIndex = 0, bIndex = 0;
@@ -120,6 +159,13 @@ public class Sort {
 		return c;
 	}
 	
+	/**
+	 * Partitions an int array based off a pivot (the right-most element)
+	 * @param a the array to partition
+	 * @param left	the left-most index of the array
+	 * @param right the right-most index of the array
+	 * @return the new index of the pivot
+	 */
 	public static int partition(int[] a, int left, int right) {
         int pivot = a[right];
         int i = left-1, j = left;
@@ -134,6 +180,13 @@ public class Sort {
         swap(a, i, right);
         return i;  
     }
+	/**
+	 * Partitions a double array based off a pivot (the right-most element)
+	 * @param a the array to partition
+	 * @param left	the left-most index of the array
+	 * @param right the right-most index of the array
+	 * @return the new index of the pivot
+	 */
 	public static int partition(double[] a, int left, int right) {
 		double pivot = a[right];
         int i = left-1, j = left;
@@ -148,6 +201,13 @@ public class Sort {
         swap(a, i, right);
         return i; 
     }
+	/**
+	 * Partitions a String array based off a pivot (the right-most element)
+	 * @param a the array to partition
+	 * @param left	the left-most index of the array
+	 * @param right the right-most index of the array
+	 * @return the new index of the pivot
+	 */
 	public static int partition(String[] a, int left, int right){
 		String pivot = a[right];
         int i = left-1, j = left;
@@ -162,8 +222,12 @@ public class Sort {
         swap(a, i, right);
         return i; 
 	}
-	
-	public static void selectionSort(int[] a){
+
+	/**
+	 * Sorts an int array using selection sort
+	 * @param a the array to sort
+	 */
+	static void selectionSort(int[] a){
 		for (int i=0; i<a.length-1; i++){
 			int index = i;
 			for(int j=i+1; j<a.length; j++)
@@ -175,6 +239,10 @@ public class Sort {
 			a[i] = smallerNumber;
 		}
 	}
+	/**
+	 * Sorts a double array using selection sort
+	 * @param a the array to sort
+	 */
 	public static void selectionSort(double[] a){
 		for (int i=0; i<a.length-1; i++){
 			int index = i;
@@ -187,6 +255,10 @@ public class Sort {
 			a[i] = smallerNumber;
 		}
 	}
+	/**
+	 * Sorts a String array using selection sort
+	 * @param a the array to sort
+	 */
 	public static void selectionSort(String[] a){
 		for (int i=0; i<a.length-1; i++){
 			int index = i;
@@ -200,6 +272,10 @@ public class Sort {
 		}
 	}
 
+	/**
+	 * Sorts an int array using insertion sort
+	 * @param a the array to sort
+	 */
 	public static void insertionSort(int[] a){
 		for(int i=1; i< a.length; i++){
 			int temp = a[i];
@@ -211,6 +287,10 @@ public class Sort {
 			a[j+1] = temp;
 		}
 	}
+	/**
+	 * Sorts a double array using insertion sort
+	 * @param a the array to sort
+	 */
 	public static void insertionSort(double[] a){
 		for(int i=1; i<a.length; i++){
 			double temp = a[i];
@@ -222,6 +302,10 @@ public class Sort {
 			a[j+1] = temp;
 		}
 	}
+	/**
+	 * Sorts a String array using insertion sort
+	 * @param a the array to sort
+	 */
 	public static void insertionSort(String[] a){
 		for(int i=1; i< a.length; i++){
 			String temp = a[i];
@@ -234,16 +318,34 @@ public class Sort {
 		}
 	}
 	
+	/**
+	 * Helper method for bubble sort with ints
+	 * @param a the array to sort
+	 */
 	public static void bubbleSort(int[] a){
 		bubbleSort(a, true, 0);
 	}
+	/**
+	 * Helper method for bubble sort with double
+	 * @param a the array to sort
+	 */
 	public static void bubbleSort(double[] a){
 		bubbleSort(a, true, 0);
 	}
+	/**
+	 * Helper method for bubble sort with Strings
+	 * @param a the array to sort
+	 */
 	public static void bubbleSort(String[] a){
 		bubbleSort(a, true, 0);
 	}
 	
+	/**
+	 * Sorts an int array using smart bubble sort
+	 * @param a the array to sort
+	 * @param swap true if there was a swap on the most recent pass, false otherwise
+	 * @param passes the total number of passes
+	 */
 	public static void bubbleSort(int[] a, boolean swap, int passes){
 		if(!swap||passes==a.length-1){
 			return;
@@ -257,6 +359,12 @@ public class Sort {
 		}
 		bubbleSort(a, swap, passes+1);
 	}
+	/**
+	 * Sorts a double array using smart bubble sort
+	 * @param a the array to sort
+	 * @param swap true if there was a swap on the most recent pass, false otherwise
+	 * @param passes the total number of passes
+	 */
 	public static void bubbleSort(double[] a, boolean swap, int passes){
 		if(!swap||passes==a.length-1){
 			return;
@@ -270,6 +378,12 @@ public class Sort {
 		}
 		bubbleSort(a, swap, passes+1);
 	}
+	/**
+	 * Sorts a String array using smart bubble sort
+	 * @param a the array to sort
+	 * @param swap true if there was a swap on the most recent pass, false otherwise
+	 * @param passes the total number of passes
+	 */
 	public static void bubbleSort(String[] a, boolean swap, int passes){
 		if(!swap||passes==a.length-1){
 			return;
@@ -284,6 +398,11 @@ public class Sort {
 		bubbleSort(a, swap, passes+1);
 	}
 
+	/**
+	 * Sorts an int array using recursive merge sort
+	 * @param a the array to sort
+	 * @return the sorted array
+	 */
 	public static int[] mergeSort(int[] a){
 		if(a.length==1){
 			return a;
@@ -308,6 +427,11 @@ public class Sort {
 		rightHalf = mergeSort(rightHalf);
 		return merge(leftHalf, rightHalf);
 	}
+	/**
+	 * Sorts a double array using recursive merge sort
+	 * @param a the array to sort
+	 * @return the sorted array
+	 */
 	public static double[] mergeSort(double[] a){
 		if(a.length==1){
 			return a;
@@ -332,6 +456,11 @@ public class Sort {
 		rightHalf = mergeSort(rightHalf);
 		return merge(leftHalf, rightHalf);
 	}
+	/**
+	 * Sorts a String array using recursive merge sort
+	 * @param a the array to sort
+	 * @return the sorted array
+	 */
 	public static String[] mergeSort(String[] a){
 		if(a.length==1){
 			return a;
@@ -357,16 +486,34 @@ public class Sort {
 		return merge(leftHalf, rightHalf);
 	}
 	
+	/**
+	 * Helper method for quick sort with ints
+	 * @param a the array to sort
+	 */
 	public static void quickSort(int[] a){
 		quickSort(a, 0, a.length-1);
 	}
+	/**
+	 * Helper method for quick sort with doubles
+	 * @param a the array to sort
+	 */
 	public static void quickSort(double[] a){
 		quickSort(a, 0, a.length-1);
 	}
+	/**
+	 * Helper method for quick sort with String
+	 * @param a the array to sort
+	 */
 	public static void quickSort(String[] a){
 		quickSort(a, 0, a.length-1);
 	}
 	
+	/**
+	 * Sorts an int array using recursive quick sort
+	 * @param a the array to sort
+	 * @param left the left-most index of the array
+	 * @param right the right-most index of the array
+	 */
 	public static void quickSort(int[] a, int left, int right){
 		int pivot = partition(a, left, right);
 		if(left<pivot-1){
@@ -376,6 +523,12 @@ public class Sort {
 			quickSort(a, pivot, right);
 		}
 	}
+	/**
+	 * Sorts a double array using recursive quick sort
+	 * @param a the array to sort
+	 * @param left the left-most index of the array
+	 * @param right the right-most index of the array
+	 */
 	public static void quickSort(double[] a, int left, int right){
 		int pivot = partition(a, left, right);
 		if(left<pivot-1){
@@ -385,6 +538,12 @@ public class Sort {
 			quickSort(a, pivot, right);
 		}
 	}
+	/**
+	 * Sorts a String array using recursive quick sort
+	 * @param a the array to sort
+	 * @param left the left-most index of the array
+	 * @param right the right-most index of the array
+	 */
 	public static void quickSort(String[] a, int left, int right){
 		int pivot = partition(a, left, right);
 		if(left<pivot-1){
